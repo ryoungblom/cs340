e0031111f5ecbf56a9c4048c25274b4ea58e1759
ry <rjyoungblom@gmail.com>
2019-03-15 16:20:09 -0700

    test

--- python/starter_website/webapp.py
+++ python/starter_website/webapp.py
@@ -43,7 +43,7 @@ def nobility():
     nbl = request.form['explNob'];
 
     query = "SELECT C.fname, C.lname, C.nobility, C.gender, C.age, H.name AS 'House', R.name AS 'Religion', C.id FROM got_characters C LEFT JOIN got_houses H ON C.house = H.id LEFT JOIN got_religions R ON C.religion = R.id WHERE C.nobility = %s;"
-    data = (nbl);
+    data = (nbl,);
     result = execute_query(db_connection, query).fetchall();
 
     query = 'SELECT id, name FROM got_houses'
